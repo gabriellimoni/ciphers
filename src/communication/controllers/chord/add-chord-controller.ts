b36e0079ac254a140c344afec3fd8187fc37a335
Gabriel Limoni <gabriel.limoni@liven.tech>
2021-01-09 14:29:19 -0300

    fix: controller receiving interface instead of class. This way we can test it more easily

--- src/communication/controllers/chord/add-chord-controller.ts
+++ src/communication/controllers/chord/add-chord-controller.ts
@@ -1,8 +1,8 @@
-import { AddChordUsecase } from '../../../usecases/add-chord-usecase'
+import { IAddChordUsecase } from '../../../usecases/add-chord-usecase'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 
 export class AddChordController implements Controller {
-  constructor (private readonly addChordUsecase: AddChordUsecase) {}
+  constructor (private readonly addChordUsecase: IAddChordUsecase) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const addedChord = await this.addChordUsecase.exec({
--- src/usecases/add-chord-usecase.ts
+++ src/usecases/add-chord-usecase.ts
@@ -3,7 +3,8 @@ import { Usecase } from './protocols'
 import { AddChordModel } from './params/chord/add-chord-param'
 import { Chord } from '../domain'
 
-export class AddChordUsecase implements Usecase<AddChordModel, Chord> {
+export interface IAddChordUsecase extends Usecase<AddChordModel, Chord> {}
+export class AddChordUsecase implements IAddChordUsecase {
   constructor (private readonly chordRepository: ChordRepository) {}
 
   async exec (chordToAdd: AddChordModel): Promise<Chord> {
