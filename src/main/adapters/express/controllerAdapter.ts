29c6d03fda84a83f7b2b53602f70b00fd4a6b7bc
Gabriel Limoni <gabrielblimoni@gmail.com>
2021-01-09 15:36:10 -0300

    feat: ensure ExpressRouteAdapter returns error messages

--- src/main/adapters/express/controllerAdapter.ts
+++ src/main/adapters/express/controllerAdapter.ts
@@ -7,6 +7,12 @@ export const adaptRoute = (controller: Controller) => {
     console.log(httpResponse.body)
     if (httpResponse.statusCode === 200) {
       return res.status(200).json(httpResponse.body)
+    } else if (httpResponse.statusCode === 400) {
+      if (httpResponse.body instanceof Error) {
+        return res.status(400).json({
+          message: httpResponse.body.message
+        })
+      }
     } else res.status(500).send('Internal server error')
   }
 }
