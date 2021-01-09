d67081d954dfc8c1ac17cc4edca88db16e2a4dab
Gabriel Limoni <gabrielblimoni@gmail.com>
2021-01-09 14:41:18 -0300

    refactor: change sut creation to factory method

--- src/communication/controllers/chord/add-chord-controller.spec.ts
+++ src/communication/controllers/chord/add-chord-controller.spec.ts
@@ -3,19 +3,36 @@ import { IAddChordUsecase } from '../../../usecases/add-chord-usecase'
 import { AddChordModel } from '../../../usecases/params/chord/add-chord-param'
 import { AddChordController } from './add-chord-controller'
 
-describe('AddChord Controller', () => {
-  test('Shoudl return 500 if AddChordUsecase throws', async () => {
-    class AddChordUsecaseStub implements IAddChordUsecase {
-      async exec (addChordModel: AddChordModel): Promise<Chord> {
-        return {
-          id: 'any_id',
-          symbol: 'any_symbol',
-          imagesUrls: ['any_iamges_urls']
-        }
+interface SutTypes {
+  sut: AddChordController
+  addChordUsecaseStub: IAddChordUsecase
+}
+
+const makeSut = (): SutTypes => {
+  const addChordUsecaseStub = makeAddChordUsecaseStub()
+  const sut = new AddChordController(addChordUsecaseStub)
+  return {
+    sut,
+    addChordUsecaseStub
+  }
+}
+
+const makeAddChordUsecaseStub = (): IAddChordUsecase => {
+  class AddChordUsecaseStub implements IAddChordUsecase {
+    async exec (addChordModel: AddChordModel): Promise<Chord> {
+      return {
+        id: 'any_id',
+        symbol: 'any_symbol',
+        imagesUrls: ['any_iamges_urls']
       }
     }
-    const addChordUsecaseStub = new AddChordUsecaseStub()
-    const sut = new AddChordController(addChordUsecaseStub)
+  }
+  return new AddChordUsecaseStub()
+}
+
+describe('AddChord Controller', () => {
+  test('Shoudl return 500 if AddChordUsecase throws', async () => {
+    const { sut, addChordUsecaseStub } = makeSut()
     jest.spyOn(addChordUsecaseStub, 'exec').mockRejectedValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const response = await sut.handle({})
     expect(response.statusCode).toBe(500)
