b07fc13e6fa9470a8f2920246710f6b5f3ea9852
Gabriel Limoni <gabrielblimoni@gmail.com>
2021-01-09 17:50:25 -0300

    feat: ensure AddCipherController returns 400 if any param is wrong

--- src/communication/controllers/cipher/add-cipher-controller.spec.ts
+++ src/communication/controllers/cipher/add-cipher-controller.spec.ts
@@ -36,10 +36,10 @@ const makeFakeCipher = (): Cipher => ({
 describe('AddCipher Controller', () => {
   test('Should return 400 if no name is provided', async () => {
     const { sut } = makeSut()
+    const cipherWithNoName = makeFakeCipher()
+    delete cipherWithNoName.name
     const response = await sut.handle({
-      body: {
-        rows: []
-      }
+      body: cipherWithNoName
     })
     expect(response).toEqual(badRequest(new RequiredParamError('name')))
   })
@@ -86,6 +86,42 @@ describe('AddCipher Controller', () => {
 
     const response = await sut.handle({
       body: cipherWithEmptyCharacter
+    })
+    expect(response).toEqual(badRequest(new InvalidParamError('rows.word.character')))
+  })
+
+  test('Should return 400 if any row does not have property "words"', async () => {
+    const { sut } = makeSut()
+
+    const cipherWithNoPropertyRow: any = makeFakeCipher()
+    cipherWithNoPropertyRow.rows.push({ otherProp: '' })
+
+    const response = await sut.handle({
+      body: cipherWithNoPropertyRow
+    })
+    expect(response).toEqual(badRequest(new InvalidParamError('rows')))
+  })
+
+  test('Should return 400 if any word does not have property "characters"', async () => {
+    const { sut } = makeSut()
+
+    const cipherWithNoPropertyWord: any = makeFakeCipher()
+    cipherWithNoPropertyWord.rows[0].words.push({ otherProp: '' })
+
+    const response = await sut.handle({
+      body: cipherWithNoPropertyWord
+    })
+    expect(response).toEqual(badRequest(new InvalidParamError('rows.word')))
+  })
+
+  test('Should return 400 if any character does not have property "char"', async () => {
+    const { sut } = makeSut()
+
+    const cipherWithNoPropertyCharacter: any = makeFakeCipher()
+    cipherWithNoPropertyCharacter.rows[0].words[0].characters.push({ otherPropr: '' })
+
+    const response = await sut.handle({
+      body: cipherWithNoPropertyCharacter
     })
     expect(response).toEqual(badRequest(new InvalidParamError('rows.word.character')))
   })
--- src/communication/controllers/cipher/add-cipher-controller.ts
+++ src/communication/controllers/cipher/add-cipher-controller.ts
@@ -12,15 +12,40 @@ export class AddCipherController implements Controller {
 
     const cipher: Cipher = httpRequest.body
     // this may take a while...
-    for (const row of cipher.rows) {
-      if (Object.keys(row).length === 0) return badRequest(new InvalidParamError('rows'))
-      for (const word of row.words) {
-        if (Object.keys(word).length === 0) return badRequest(new InvalidParamError('rows.word'))
-        for (const character of word.characters) {
-          if (Object.keys(character).length === 0) return badRequest(new InvalidParamError('rows.word.character'))
-        }
+    const rowError = checkForRowErrors(cipher)
+    if (rowError) return badRequest(rowError)
+    const wordError = checkForWordErrors(cipher)
+    if (wordError) return badRequest(wordError)
+    const charError = checkForCharacterErrors(cipher)
+    if (charError) return badRequest(charError)
+
+    return null
+  }
+}
+
+function checkForRowErrors (cipher: Cipher): Error | undefined {
+  for (const row of cipher.rows) {
+    if (Object.keys(row).length === 0) return new InvalidParamError('rows')
+    if (!Object.keys(row).includes('words')) return new InvalidParamError('rows')
+  }
+}
+
+function checkForWordErrors (cipher: Cipher): Error | undefined {
+  for (const row of cipher.rows) {
+    for (const word of row.words) {
+      if (Object.keys(word).length === 0) return new InvalidParamError('rows.word')
+      if (!Object.keys(word).includes('characters')) return new InvalidParamError('rows.word')
+    }
+  }
+}
+
+function checkForCharacterErrors (cipher: Cipher): Error | undefined {
+  for (const row of cipher.rows) {
+    for (const word of row.words) {
+      for (const character of word.characters) {
+        if (Object.keys(character).length === 0) return new InvalidParamError('rows.word.character')
+        if (!Object.keys(word).includes('char')) return new InvalidParamError('rows.word.character')
       }
     }
-    return null
   }
 }
