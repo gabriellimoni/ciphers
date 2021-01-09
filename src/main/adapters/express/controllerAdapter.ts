4785ddc6dd2fc9578a88d92056feccfb5fa50d2a
Gabriel Limoni <gabrielblimoni@gmail.com>
2021-01-09 16:21:16 -0300

    fix: fix ExpressAdapter to return error when status > 400

--- src/main/adapters/express/controllerAdapter.ts
+++ src/main/adapters/express/controllerAdapter.ts
@@ -7,9 +7,9 @@ export const adaptRoute = (controller: Controller) => {
     console.log(httpResponse.body)
     if (httpResponse.statusCode === 200) {
       return res.status(200).json(httpResponse.body)
-    } else if (httpResponse.statusCode === 400) {
+    } else if (httpResponse.statusCode > 400) {
       if (httpResponse.body instanceof Error) {
-        return res.status(400).json({
+        return res.status(httpResponse.statusCode).json({
           message: httpResponse.body.message
         })
       }
